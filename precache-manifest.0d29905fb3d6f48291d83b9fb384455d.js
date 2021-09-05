c90c5dcea28c4aa98dcbe34726c11a669802e28c
yaroslavoz <yaroslavozv@gmail.com>
2021-09-05 22:34:55 +0300

    Updates

---
+++ precache-manifest.0d29905fb3d6f48291d83b9fb384455d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0432b99319fbfb2c76c8861aaa56794",
+    "url": "/Trello-Task/index.html"
+  },
+  {
+    "revision": "bd9279527def248b5071",
+    "url": "/Trello-Task/static/js/2.04f16588.chunk.js"
+  },
+  {
+    "revision": "529b4df5d70649de063c",
+    "url": "/Trello-Task/static/js/main.2a839ba4.chunk.js"
+  },
+  {
+    "revision": "3b71ce59c89541bc5d33",
+    "url": "/Trello-Task/static/js/runtime~main.1024717e.js"
+  }
+]);
